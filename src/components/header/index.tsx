a537e61d09fc65ad3121c16f1a5fbcf8808f45b5
Merve Ozturk <ozturkmerve745@gmail.com>
2024-03-13 22:08:01 +0300

    Added categories

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,39 +3,56 @@ import styles from './index.module.css';
 
 const Header = () => {
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.combinedContainer}>
-        <div className={styles.logo}>
-          <a href='#'>Logo</a>
+    <div>
+      <nav className={styles.navbar}>
+        <div className={styles.combinedContainer}>
+          <div className={styles.logo}>
+            <a href='#'>Logo</a>
+          </div>
+          <div className={styles.searchContainer}>
+            <input
+              className={styles.searchInput}
+              type='search'
+              name='search'
+              placeholder='Search here'
+              autoComplete='off'
+            />
+          </div>
         </div>
-        <div className={styles.searchContainer}>
-          <input
-            className={styles.searchInput}
-            type='search'
-            name='search'
-            placeholder='Search here'
-            autoComplete='off'
-          />
-          {/*<button className={styles.searchButton}>Search</button> */}
+
+        <div className={styles.navbarLinks}>
+          <ul className={styles.primaryLinks}>
+            <li>
+              <a href='#'>Mağaza</a>
+            </li>
+            <li>
+              <a href='#'>Hesap</a>
+            </li>
+            <li>
+              <a href='#'>Favoriler</a>
+            </li>
+            <li>
+              <a href='#'>Sepet</a>
+            </li>
+          </ul>
         </div>
+      </nav>
+
+      <div className={styles.categoryLinks}>
+        <a href='#'>KADIN</a>
+        <a href='#'>ERKEK</a>
+        <a href='#'>ÇOCUK</a>
+        <a href='#'>BEBEK</a>
+        <a href='#'>FIRSATLAR</a>
+        <a href='#'>SÜRDÜRÜLEBİLİRLİK</a>
+        <a href='#'>KADIN</a>
+        <a href='#'>ERKEK</a>
+        <a href='#'>ÇOCUK</a>
+        <a href='#'>BEBEK</a>
+        <a href='#'>FIRSATLAR</a>
+        <a href='#'>SÜRDÜRÜLEBİLİRLİK</a>
       </div>
-      <div className={styles.navbarLinks}>
-        <ul>
-          <li>
-            <a href='#'>Mağaza</a>
-          </li>
-          <li>
-            <a href='#'>Hesap</a>
-          </li>
-          <li>
-            <a href='#'>Favoriler</a>
-          </li>
-          <li>
-            <a href='#'>Sepet</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    </div>
   );
 };
 
