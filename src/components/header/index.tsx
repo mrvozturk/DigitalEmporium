f2a82565d4cd6139d11c35cccc06d75134f605a7
Merve Ozturk <ozturkmerve745@gmail.com>
2024-03-21 15:24:20 +0300

    update

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,97 +5,47 @@ import {
   faUser,
   faHeart,
   faShoppingCart,
-  faTimes,
-  faBars,
   faSearch
 } from '@fortawesome/free-solid-svg-icons';
 import styles from './index.module.css';
+
 import SideBar from './sideBar';
 
 const Header = () => {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
-  const [isMobileView, setMobileView] = useState(false);
   const [isSearchOpen, setSearchOpen] = useState(false);
 
   const toggleSidebar = () => {
     setSidebarOpen(!isSidebarOpen);
   };
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setMobileView(window.innerWidth < 770);
-
-      const handleResize = () => {
-        setMobileView(window.innerWidth < 770);
-      };
-      window.addEventListener('resize', handleResize);
-
-      return () => window.removeEventListener('resize', handleResize);
-    }
-  }, []);
-
   return (
     <div>
       <nav className={styles.navbar}>
-        <div className={styles.combinedContainer}>
-          <div className={styles.buttonContainer}>
-            <button className={styles.sidebarToggle} onClick={toggleSidebar}>
-              {!isSidebarOpen ? <FontAwesomeIcon icon={faBars} /> : null}
-            </button>
-          </div>
-          <div className={styles.logo}>
-            <a href='/'>Logo</a>
-          </div>
-          {isMobileView && isSidebarOpen && (
-            <button
-              className={`${styles.sidebarToggle} fa-times`}
-              onClick={toggleSidebar}
-            >
-              <FontAwesomeIcon icon={faTimes} />
-            </button>
-          )}
-          
-          <div className={styles.searchContainer}>
-            {(isMobileView && isSearchOpen) || !isMobileView ? (
-              <input
-                className={styles.searchInput}
-                type='search'
-                name='search'
-                placeholder={isMobileView ? ' Ara' : 'Ara...'}
-                autoComplete='off'
-              />
-            ) : null}
-          </div>
+        <div className={styles.logo}>
+          <a href='/'>Logo</a>
         </div>
 
         <div className={styles.navbarLinks}>
-          <ul className={styles.primaryLinks}>
+          <ul>
             <li>
-              <a href='#' onClick={() => setSearchOpen(!isSearchOpen)}>
-                {isMobileView ? <FontAwesomeIcon icon={faSearch} /> : ''}
+              <a href='#'>
+                <FontAwesomeIcon icon={faSearch} />
               </a>
             </li>
             <li>
               <a href='#'>
-                {isMobileView ? <FontAwesomeIcon icon={faUser} /> : 'Hesap'}
+                <FontAwesomeIcon icon={faUser} />
               </a>
             </li>
             <li>
               <a href='#'>
-                {isMobileView ? (
-                  <FontAwesomeIcon icon={faHeart} />
-                ) : (
-                  'Favoriler'
-                )}
+                <FontAwesomeIcon icon={faUser} />
               </a>
             </li>
             <li>
               <a href='#'>
-                {isMobileView ? (
-                  <FontAwesomeIcon icon={faShoppingCart} />
-                ) : (
-                  'Sepet'
-                )}
+                <FontAwesomeIcon icon={faUser} />
               </a>
             </li>
           </ul>
--- src/components/header/sideBar.tsx
+++ src/components/header/sideBar.tsx
@@ -1,23 +1,13 @@
 'use client';
-import React, { useEffect, useRef } from 'react';
-import styles from './index.module.css';
+import React, {  useEffect, useRef } from 'react';
+import styles from './sideBar.module.css';
 
-const SideBar = ({
-  toggleSidebar,
-  isSidebarOpen
-}: {
-  toggleSidebar: () => void;
-  isSidebarOpen: boolean;
-}) => {
+const SideBar = ({ toggleSidebar, isSidebarOpen }: { toggleSidebar: () => void; isSidebarOpen: boolean; }) => {
   const sidebarRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (
-        sidebarRef.current &&
-        !sidebarRef.current.contains(event.target as HTMLElement) &&
-        isSidebarOpen
-      ) {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target as HTMLElement) && isSidebarOpen) {
         toggleSidebar();
       }
     };
@@ -30,43 +20,20 @@ const SideBar = ({
   }, [isSidebarOpen, toggleSidebar]);
 
   return (
-    <div
-      className={`${styles.sidebar} ${isSidebarOpen ? styles.sidebarOpen : ''}`}
-      ref={sidebarRef}
-    >
+    <div className={`${styles.sidebar} ${isSidebarOpen ? styles.sidebarOpen : ''}`} ref={sidebarRef}>
       <div className={styles.categoryLinks}>
         <button className={styles.closeButton} onClick={toggleSidebar}></button>
         <ul>
-          <li>
-            <a href='#'>KADIN</a>
-          </li>
-          <li>
-            <a href='#'>ERKEK</a>
-          </li>
-          <li>
-            <a href='#'>ÇOCUK</a>
-          </li>
-          <li>
-            <a href='#'>KADIN</a>
-          </li>
-          <li>
-            <a href='#'>ERKEK</a>
-          </li>
-          <li>
-            <a href='#'>ÇOCUK</a>
-          </li>
-          <li>
-            <a href='#'>KADIN</a>
-          </li>
-          <li>
-            <a href='#'>ERKEK</a>
-          </li>
-          <li>
-            <a href='#'>ÇOCUK</a>
-          </li>
-          <li>
-            <a href='#'>BEBEK</a>
-          </li>
+          <li><a href='#'>KADIN</a></li>
+          <li><a href='#'>ERKEK</a></li>
+          <li><a href='#'>ÇOCUK</a></li>
+          <li><a href='#'>KADIN</a></li>
+          <li><a href='#'>ERKEK</a></li>
+          <li><a href='#'>ÇOCUK</a></li>
+          <li><a href='#'>KADIN</a></li>
+          <li><a href='#'>ERKEK</a></li>
+          <li><a href='#'>ÇOCUK</a></li>
+          <li><a href='#'>BEBEK</a></li>
         </ul>
       </div>
     </div>
