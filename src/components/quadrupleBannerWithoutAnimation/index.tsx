f55944b3fa26dc16fa719da7810b06aba8d27a01
Merve Ozturk <ozturkmerve745@gmail.com>
2024-04-25 22:36:00 +0300

    css update

--- src/components/quadrupleBannerWithoutAnimation/index.tsx
+++ src/components/quadrupleBannerWithoutAnimation/index.tsx
@@ -6,7 +6,6 @@ import styles from './index.module.css';
 
 const images = [
   'https://ktnimg2.mncdn.com/cms/2024/04/18/1490aa22-333e-41e6-b1c1-4207beaadc40.png',
-
   'https://ktnimg2.mncdn.com/cms/2024/03/18/78056947-f73e-43ac-bfc1-000ca6756df6.png',
   'https://ktnimg2.mncdn.com/cms/2024/03/28/df302119-8a71-4583-afa8-7324f4acfdbf.png',
   'https://ktnimg2.mncdn.com/cms/2024/03/18/f5d6294a-d017-42fc-a3b7-109e112e5666.png'
