07c21f4bc5f853d46906761c9c3217518afab026
Merve Ozturk <ozturkmerve745@gmail.com>
2024-05-02 16:23:12 +0300

    login page created and css created

--- src/components/accountProcess/index.tsx
+++ src/components/accountProcess/index.tsx
@@ -1,8 +1,64 @@
 'use client';
 import React from 'react';
+import styles from './index.module.css';
 
 const AccountProcess: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      {/* Kayıt Olma Formu */}
+      <div className={styles.formContainer}>
+        <h2>Kayıt Ol</h2>
+
+        <form className={styles.loginForm}>
+          <div>
+            <input type='email' name='email' placeholder='E-posta Adresi*' />
+          </div>
+          <div>
+            <input type='text' name='firstName' placeholder='Ad*' />
+          </div>
+          <div>
+            <input type='text' name='lastName' placeholder='Soyad*' />
+          </div>
+          <div>
+            <input type='password' name='password' placeholder='Şifre*' />
+          </div>
+          <div>
+            <input
+              type='tel'
+              name='phoneNumber'
+              placeholder='Telefon Numarası*'
+            />
+          </div>
+          <div className={styles.dateContainer}>
+            <div>
+              <input type='number' name='day' placeholder='Gün*' />
+            </div>
+            <div>
+              <input type='number' name='month' placeholder='Ay*' />
+            </div>
+            <div>
+              <input type='number' name='year' placeholder='Yıl' />
+            </div>
+          </div>
+          <button type='submit'>Kayıt Ol</button>
+        </form>
+      </div>
+
+      {/* Giriş Yapma Formu */}
+      <div className={styles.loginFormContainer}>
+        <h2>Giriş Yap</h2>
+        <form className={styles.loginForm}>
+          <div>
+            <input type='email' name='email' placeholder='E-posta Adresi*' />
+          </div>
+          <div>
+            <input type='password' name='password' placeholder='Şifre*' />
+          </div>
+          <button type='submit'>Giriş Yap</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AccountProcess;
