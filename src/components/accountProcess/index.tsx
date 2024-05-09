b91f6240b2538ff8019e04cd45612ed71a1484ce
Merve Ozturk <ozturkmerve745@gmail.com>
2024-05-09 16:35:47 +0300

    updated

--- src/components/accountProcess/index.tsx
+++ src/components/accountProcess/index.tsx
@@ -2,38 +2,53 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Göz simgeleri için
 import styles from './index.module.css';
 
 const AccountProcess: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [showRegistrationPassword, setShowRegistrationPassword] = useState<
+    boolean
+  >(false);
+  const [showLoginPassword, setShowLoginPassword] = useState<boolean>(false);
+
+  const togglePasswordVisibility = (type: 'registration' | 'login') => {
+    if (type === 'registration') {
+      setShowRegistrationPassword(!showRegistrationPassword);
+    } else {
+      setShowLoginPassword(!showLoginPassword);
+    }
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.formContainer}>
         <h2>Kayıt Ol</h2>
-
         <form className={styles.registrationForm}>
-          <div>
-            <input type='email' name='email' placeholder='E-posta Adresi*' />
-          </div>
+          <input type='email' name='email' placeholder='E-posta Adresi*' />
           <div className={styles.nameContainer}>
-            <div>
-              <input type='text' name='firstName' placeholder='Ad*' />
-            </div>
-            <div>
-              <input type='text' name='lastName' placeholder='Soyad*' />
-            </div>
+            <input type='text' name='firstName' placeholder='Ad*' />
+            <input type='text' name='lastName' placeholder='Soyad*' />
           </div>
-          <div>
-            <input type='password' name='password' placeholder='Şifre*' />
-          </div>
-          <div>
+          <div className={styles.passwordInputContainer}>
             <input
-              type='tel'
-              name='phoneNumber'
-              placeholder='Telefon Numarası*'
+              className={styles.passwordWrapper}
+              type={showRegistrationPassword ? 'text' : 'password'}
+              name='password'
+              placeholder='Şifre*'
             />
+            <button
+              type='button'
+              onClick={() => togglePasswordVisibility('registration')}
+            >
+              {showRegistrationPassword ? <FaEyeSlash /> : <FaEye />}
+            </button>
           </div>
+          <input
+            type='tel'
+            name='phoneNumber'
+            placeholder='Telefon Numarası*'
+          />
           <label htmlFor='birthdate'>Doğum Tarihi</label>
 
           <div className={styles.dateContainer}>
@@ -46,34 +61,41 @@ const AccountProcess: React.FC = () => {
               showYearDropdown
               dropdownMode='scroll'
               minDate={new Date(1950, 0, 1)} // 1 Ocak 1950
-              maxDate={new Date(2008, 11, 31)} // 31 Aralık 2008
+              maxDate={new Date(2024, 11, 31)} // 31 Aralık 2008
               scrollableMonthYearDropdown
               scrollableYearDropdown
-              yearDropdownItemNumber={58}
+              yearDropdownItemNumber={78}
             />
           </div>
-
-          {/* Cinsiyet Seçimi */}
+          <label htmlFor='birthdate'>Cinsiyet</label>
           <div className={styles.genderContainer}>
-            <div className={styles.genderName}>Cinsiyet</div>
             <input type='radio' name='gender' value='female' />
             <label> Kadın </label>
             <input type='radio' name='gender' value='male' />
-            <label> Erkek</label>{' '}
+            <label> Erkek</label>
+            <input type='radio' name='gender' value='other' />
+            <label> Diğer</label>
           </div>
+
           <button type='submit'>Kayıt Ol</button>
         </form>
       </div>
-
-      {/* Giriş Yapma Formu */}
       <div className={styles.loginFormContainer}>
         <h2>Giriş Yap</h2>
         <form className={styles.loginForm}>
+          <input type='email' name='email' placeholder='E-posta Adresi*' />
           <div>
-            <input type='email' name='email' placeholder='E-posta Adresi*' />
-          </div>
-          <div>
-            <input type='password' name='password' placeholder='Şifre*' />
+            <input
+              type={showLoginPassword ? 'text' : 'password'}
+              name='password'
+              placeholder='Şifre*'
+            />
+            <button
+              type='button'
+              onClick={() => togglePasswordVisibility('login')}
+            >
+              {showLoginPassword ? <FaEyeSlash /> : <FaEye />}
+            </button>
           </div>
           <button type='submit'>Giriş Yap</button>
         </form>
