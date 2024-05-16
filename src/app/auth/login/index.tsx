7c8766302d886532b1080bea121c934ce71f7e19
Onat Varis <onatvaris@gmail.com>
2024-05-16 23:03:58 +0300

    ✨  account-process [ADDED] login form content component create

---
+++ src/app/auth/login/index.tsx
@@ -0,0 +1,39 @@
+'use client';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import styles from './login.module.css';
+
+const LoginForm: React.FC = () => {
+  const [showLoginPassword, setShowLoginPassword] = useState<boolean>(false);
+
+  return (
+    <div className={styles.loginFormContainer}>
+      <h2>Giriş Yap</h2>
+      <form className={styles.loginForm}>
+        <input type='email' name='email' placeholder='E-posta Adresi*' />
+        <div className={styles.passwordInputContainer}>
+          <input
+            className={styles.passwordInput}
+            type={showLoginPassword ? 'text' : 'password'}
+            name='password'
+            placeholder='Şifre*'
+          />
+          <button
+            type='button'
+            onClick={() => setShowLoginPassword(prevState => !prevState)}
+            className={styles.eyeButton}
+          >
+            {showLoginPassword ? <FaEyeSlash /> : <FaEye />}
+          </button>
+        </div>
+        <button type='submit' className={styles.loginButton}>
+          GİRİŞ YAP
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
