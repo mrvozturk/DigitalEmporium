34aea617391abce6caa720cc5f53d8bbf1c80fb7
Merve Ozturk <ozturkmerve745@gmail.com>
2024-11-28 16:27:03 +0300

    css in mainimage was updated with talwind css

--- src/components/mainImage/index.tsx
+++ src/components/mainImage/index.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import Image from 'next/image';
-import styles from './index.module.css';
-import { FiltersType } from '@/app/product/[productId]/page';
 
 const MainImage: React.FC<{
   initialPhoto: string;
-  filters: FiltersType;
+  filters: { imageUrl?: string };
 }> = ({ initialPhoto, filters }) => {
   const selectedImage = filters.imageUrl || initialPhoto;
 
   return (
-    <div className={styles.imageContainer}>
+    <div
+      className=' xs:hidden
+         sm:flex flex-1 basis-1/4 items-start justify-center m-2
+        sm:basis-[45%] md:basis-[35%] lg:basis-1/4
+        max-w-[25vw]
+      '
+    >
       <Image
         src={selectedImage}
-        alt={selectedImage}
+        alt='Main Product'
         width={600}
         height={700}
-        className={styles.productImage}
+        className='
+          w-full max-w-[25vw] max-h-[25vw] object-contain p-2 border border-gray-200 bg-gray-200
+          xs:block
+        '
       />
     </div>
   );
