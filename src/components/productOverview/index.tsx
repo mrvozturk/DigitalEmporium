4f4a31d767d819786683693e2e7af985342233a4
Merve Ozturk <ozturkmerve745@gmail.com>
2024-12-04 18:22:09 +0300

    ProductOverview css rearranged with talwind

--- src/components/productOverview/index.tsx
+++ src/components/productOverview/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { AiFillStar } from 'react-icons/ai';
 import { Product } from '../../lib/data';
-import styles from './index.module.css';
 
 interface ProductOverviewProps {
   productDetail: Product;
@@ -21,25 +20,30 @@ const ProductOverview: React.FC<ProductOverviewProps> = ({
   showProductDivider = false
 }) => {
   return (
-    <>
+    <div className=' xs:block hidden'>
       {/* Main Product Info Section */}
       {!showDetailsSection &&
         !showPurchaseSection &&
         !showPriceSection &&
         !showProductDivider && (
-          <div className={styles.productContainer}>
-            <div className={styles.ratingContainer}>
-              <p className={styles.brandName}>{productDetail.brand}</p>
-              <div className={styles.ratingDetails}>
-                <span className={styles.ratingScore}>
+          <div className='w-full px-4'>
+            <div className='flex justify-between items-center w-full'>
+              <p
+                className='text-xs font-medium  mt-2 mr-2 '
+                style={{ color: '#007185' }}
+              >
+                {productDetail.brand}
+              </p>
+              <div className='flex items-center mt-2'>
+                <span className='text-xs mr-2' style={{ color: '#007185' }}>
                   {productDetail.rating.rate.toFixed(1)}
                 </span>
-                <div className={styles.ratingStars}>
+                <div className='flex items-center'>
                   {[...Array(5)].map((_, index) => (
                     <a
                       href='#review'
-                      key={`${productDetail.title}-star-${index}`} 
-                      className={styles.starLink}
+                      key={`${productDetail.title}-star-${index}`}
+                      className='text-md'
                     >
                       <AiFillStar
                         color={index < starRating ? '#ffc107' : '#e4e5e9'}
@@ -49,28 +53,30 @@ const ProductOverview: React.FC<ProductOverviewProps> = ({
                 </div>
               </div>
             </div>
-            <h1 className={styles.productTitleMobile}>{productDetail.title}</h1>
+            <h1 className='text-base font-medium text-gray-700 mt-2'>
+              {productDetail.title}
+            </h1>
           </div>
         )}
 
       {/* Price Section */}
       {showPriceSection && (
-        <p>
-          <span className={styles.priceText}>{productDetail.price}</span>
+        <p className='text-2xl font-semibold text-gray-900 mt-4'>
+          {productDetail.price}
         </p>
       )}
 
       {/* Divider Section */}
-      {showProductDivider && <hr className={styles.productDivider} />}
+      {showProductDivider && <hr className='my-4 border-t border-gray-300' />}
 
       {/* Purchase Section */}
       {showPurchaseSection && (
-        <div className={styles.purchaseSection}>
-          <p className={styles.stockStatus}>In Stock</p>
-          <div className={styles.quantityWrapper}>
+        <div className='w-full p-1 mt-4 rounded-lg'>
+          <p className='text-green-700 text-lg font-medium mb-2'>In Stock</p>
+          <div className='w-full mb-4'>
             <select
               id='quantity'
-              className={styles.quantityDropdown}
+              className='w-full px-4 py-3 border border-gray-300 rounded-lg bg-gray-200 text-base'
               defaultValue=''
             >
               <option value='' disabled hidden>
@@ -83,28 +89,40 @@ const ProductOverview: React.FC<ProductOverviewProps> = ({
               ))}
             </select>
           </div>
-          <button className={styles.cartButton}>Add to Cart</button>
-          <button className={styles.purchaseButton}>Buy Now</button>
+          <button className='w-full py-3 bg-yellow-400 text-black font-medium rounded-lg hover:border-4 hover:border-black-500'>
+            Add to Cart
+          </button>
+          <button className='w-full py-3 bg-orange-500 text-black font-medium rounded-lg mt-4 hover:border-4 hover:border-black-500'>
+            Buy Now
+          </button>
         </div>
       )}
 
       {/* Details Section */}
       {showDetailsSection && (
-        <div className={styles.detailsSection}>
-          <h2 className={styles.detailsTitle}>Product details</h2>
-          <div className={styles.detailItem}>
-            <strong>Fabric type</strong> <span>{productDetail.fabricType}</span>
+        <div className='flex flex-col gap-4 mt-4'>
+          <h2 className='text-xl font-bold text-gray-900'>Product details</h2>
+          <div className='flex flex-col text-base mb-4'>
+            <strong className='text-md text-gray-900'>Fabric type</strong>
+            <span className='text-sm text-gray-700'>
+              {productDetail.fabricType}
+            </span>
           </div>
-          <div className={styles.detailItem}>
-            <strong>Care instructions</strong>{' '}
-            <span>{productDetail.careInstructions}</span>
+          <div className='flex flex-col text-base mb-4'>
+            <strong className='text-md text-gray-900'>Care instructions</strong>
+            <span className='text-sm text-gray-700'>
+              {productDetail.careInstructions}
+            </span>
           </div>
-          <div className={styles.detailItem}>
-            <strong>Origin</strong> <span>{productDetail.origin}</span>
+          <div className='flex flex-col text-base mb-4'>
+            <strong className='text-md text-gray-900'>Origin</strong>
+            <span className='text-sm text-gray-700'>
+              {productDetail.origin}
+            </span>
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
