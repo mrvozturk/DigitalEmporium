312fe54e509457f17253c8a555e3822666879070
Merve Ozturk <ozturkmerve745@gmail.com>
2024-12-09 17:25:58 +0300

    Talwind css was edited in ImageSwiper

--- src/components/swiperImage/index.tsx
+++ src/components/swiperImage/index.tsx
@@ -61,7 +61,7 @@ const ImageSwiper = ({ productDetail, colors }: ImageSwiperProps) => {
   }, []);
 
   return (
-    <div className='w-full flex flex-col justify-center items-center p-0 xs:bg-[#f6f4f4] mt-2 '>
+    <div className='w-full flex flex-col justify-center items-center p-0 xs:bg-[#f6f4f4] xs:mt-2 '>
       <div
         className='flex snap-x snap-mandatory overflow-x-auto scroll-smooth px-0 w-full sm:flex-col gap-2 items-center hide-scrollbar'
         id='imageThumbnails'
@@ -74,7 +74,7 @@ const ImageSwiper = ({ productDetail, colors }: ImageSwiperProps) => {
             alt={productDetail.title}
             width={340}
             height={352}
-            className={`w-full flex-shrink-0 snap-center aspect-[4/5] object-contain object-center cursor-pointer align-center p-3
+            className={`w-full flex-shrink-0 snap-center aspect-[4/5] object-contain object-center cursor-pointer align-center xs:p-3
               xs:w-full xs:aspect-square xs:object-contain 
               sm:w-[70px] sm:h-[50px] sm:border sm:border-[#b8b4b4] sm:rounded-md sm:shadow-lg 
               md:w-[55px] md:h-[45px]
@@ -84,7 +84,7 @@ const ImageSwiper = ({ productDetail, colors }: ImageSwiperProps) => {
         ))}
       </div>
       <div
-        className='flex justify-center items-center  xs:bg-white xs:border xs:bg-[#f6f4f4]  xs:py-4 xs:w-full sm:hidden mb-3'
+        className='flex justify-center items-center  xs:bg-white xs:border xs:bg-[#f6f4f4]  xs:py-4 xs:w-full sm:hidden xs:mb-3'
         id='paginationDots'
       >
         {productDetail.photos.map((_, index) => (
