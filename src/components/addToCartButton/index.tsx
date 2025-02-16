5722a42f0f512784ab0dadff64b83126b87a1993
Merve Ozturk <ozturkmerve745@gmail.com>
2025-02-16 19:41:22 +0300

    css updated

--- src/components/addToCartButton/index.tsx
+++ src/components/addToCartButton/index.tsx
@@ -14,8 +14,11 @@ export function AddToCartButton({ id, src }: { id: string; src: string }) {
   };
 
   return (
-    <button onClick={handleClick} className='bg-blue-500 text-white px-4 py-2 rounded'>
-      Sepete Ekle
+    <button
+      className='bg-black text-white px-4 py-2 rounded-full hover:bg-gray-800'
+      onClick={handleClick}
+    >
+      Add to Cart
     </button>
   );
 }
