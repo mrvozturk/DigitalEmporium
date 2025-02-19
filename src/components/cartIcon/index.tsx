d3acf595fe18d9334df2ea98100a4625a5f7d30e
Merve Ozturk <ozturkmerve745@gmail.com>
2025-02-19 19:46:54 +0300

    Sidecart added to cart, short-term pop-up created and css edited

--- src/components/cartIcon/index.tsx
+++ src/components/cartIcon/index.tsx
@@ -1,21 +1,39 @@
 'use client';
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/lib/store';
 import Link from 'next/link';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
+import SideCart from './../sideCart';
 
 const CartIcon = () => {
   const cartItems = useSelector((state: RootState) => state.cart.items);
+  const [isOpen, setIsOpen] = useState(false);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  const handleMouseEnter = () => {
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    setIsOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => {
+      setIsOpen(false);
+    }, 5000);
+  };
 
   return (
-    <div className='relative'>
+    <div
+      className='relative'
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {/* Sepet İkonu */}
       <Link
         href='/cart'
         className='flex items-center hover:text-gray-600 relative'
       >
         <span className='hidden md:block mr-6'>Sepet [{cartItems.length}]</span>
-
         <div className='relative block md:hidden'>
           <AiOutlineShoppingCart className='text-xl' />
           {cartItems.length > 0 && (
@@ -25,6 +43,13 @@ const CartIcon = () => {
           )}
         </div>
       </Link>
+
+      {/* SideCart */}
+      {isOpen && (
+        <div className='absolute top-12 right-0 w-[350px] z-50'>
+          <SideCart isOpen={isOpen} onClose={() => setIsOpen(false)} />
+        </div>
+      )}
     </div>
   );
 };
