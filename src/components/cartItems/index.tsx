089c91343bb8be07e39c898224afcc216b537b71
Merve Ozturk <ozturkmerve745@gmail.com>
2025-02-20 15:22:14 +0300

    Cart products rearranged on large screen, carttems file updated

--- src/components/cartItems/index.tsx
+++ src/components/cartItems/index.tsx
@@ -4,13 +4,14 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '@/lib/store';
 import Image from 'next/image';
-import { AiOutlineShopping, AiOutlineClose } from 'react-icons/ai';
+import { AiOutlineShopping, AiOutlineDelete } from 'react-icons/ai';
 
-import { removeFromCart } from '@/lib/features/cart/cartSlice';
+import { removeFromCart, updateQuantity } from '@/lib/features/cart/cartSlice';
 
 const CartItems = () => {
   const cartItems = useSelector((state: RootState) => state.cart.items);
   const dispatch = useDispatch();
+
   const total = cartItems.reduce((acc, item) => {
     const cleanedPrice = item.price
       .replace(/[^\d,]/g, '') // Sadece rakam ve virgülü tut
@@ -46,12 +47,16 @@ const CartItems = () => {
         <div className='flex flex-col lg:flex-row gap-20 p-2 sm:p-4 md:p-6 lg:p-2 xs:gap-10 sm:gap-10 md:gap-10 lg:gap-20'>
           {/*  Sepet Ürünleri */}
           <div className='flex-1 mt-2'>
-            <div className='flex justify-between items-center border-b border-gray-300 pb-4'>
-              <h1 className='text-lg font-bold'>Sepetim</h1>
-              <a href='/' className='text-xs text-black-600 underline'>
-                Alışverişe Devam Et
-              </a>
+            <div className='flex items-center justify-start border-b pb-2 mb-4'>
+              <h1 className='text-lg font-bold'>
+                SEPETİM ({cartItems.length})
+              </h1>
+
+              <span className='ml-4 text-gray-400 font-semibold'>
+                FAVORİLERİM
+              </span>
             </div>
+
             {cartItems.map(product => (
               <div
                 key={product.id}
@@ -69,52 +74,67 @@ const CartItems = () => {
 
                 {/* Ürün Bilgileri */}
                 <div className='l w-full ml-2 '>
-                  <div>
-                    <p className=' text-xxs text-start  '>{product.title}</p>
+                  <div className='flex flex-col flex-1 ml-1 text-start'>
+                    <p className='text-xs xs:text-xxs sm:text-xxs md:text-xxs lg:text-xs  '>
+                      {product.title}
+                    </p>
+                    <p className='text-xxs lg:text-xs xs:text-xxs sm:text-xxs md:text-xxs font-bold mt-2 '>
+                      {product.price}
+                    </p>
+                    <p className='text-xs mt-2'>
+                      <span className='font-bold'> Beden: </span>
+                    </p>
+                    <p className='text-xs'>
+                      <span className='font-bold'> Renk:</span>
+                    </p>
                   </div>
-                  <a
-                    href='#'
-                    className='flex flex-col text-xs text-black-600 underline mb-2 xs:text-xxs text-start '
+                </div>
+
+                {/* Adet Güncelleme ve Çöp Kutusu */}
+                <div className='flex flex-col items-center gap-4'>
+                  {/* Çöp Kutusu Butonu */}
+                  <button
+                    onClick={() => dispatch(removeFromCart(product.id))}
+                    className='text-black-500 hover:text-red-500  xs:ml-20 sm:ml-20 md:ml-20 lg:ml-15'
                   >
-                    Düzenle
-                  </a>
-                  <div className='flex flex-col w-full'>
-                    <div className='  text-xs mt-1'>
-                      <p className=' flex font-bold mr-2 xs:mr-3 xs:text-xxs'>
-                        Renk:
-                      </p>
-                      <p className=' flex font-bold mr-2 xs:mr-3 xs:text-xxs xs:mb-3 sm:mb-0 md:mb-2 lg:mb-0'>
-                        Beden:
-                      </p>{' '}
-                    </div>
-                    <div className='flex flex-row  xs:flex-row md:gap-4 lg:gap-2.5 w-full lg:justify-end lg:flex-row lg:items-center xs:mt-0 sm:mt-0 md:mt-0 lg:mt-0 '>
-                      <select
-                        value={product.quantity}
-                        onChange={e => {
-                          const newQuantity = Number(e.target.value);
-                        }}
-                        className='border px-2 py-1 text-xs xs:py-1 md:py-1 lg:px-2 lg:py-1 w-full xs:w-11  md:w-12 lg:w-40 lg:h-9 max-w-[60px] bg-white-100 text-white-900 border-black-400 '
-                      >
-                        {[1, 2, 3, 4, 5].map(num => (
-                          <option key={num} value={num}>
-                            {num}
-                          </option>
-                        ))}
-                      </select>
-                    </div>
+                    <AiOutlineDelete size={18} />
+                  </button>
+
+                  {/* Adet Güncelleme Butonları */}
+                  <div className='flex items-center space-x-2 mt-12'>
+                    <button
+                      className='bg-gray-200 px-2 py-0.2 rounded disabled:opacity-50 '
+                      onClick={() =>
+                        product.quantity > 1 &&
+                        dispatch(
+                          updateQuantity({
+                            id: product.id,
+                            quantity: product.quantity - 1
+                          })
+                        )
+                      }
+                      disabled={product.quantity <= 1}
+                    >
+                      -
+                    </button>
+                    <span className='px-3 font-semibold'>
+                      {product.quantity}
+                    </span>
+                    <button
+                      className='bg-gray-200 px-2 py-0.2  rounded'
+                      onClick={() =>
+                        dispatch(
+                          updateQuantity({
+                            id: product.id,
+                            quantity: product.quantity + 1
+                          })
+                        )
+                      }
+                    >
+                      +
+                    </button>
                   </div>
                 </div>
-                <div className='flex items-center  mt-2 xs:mb-3 xs:mt-20 sm:mt-2 md:mt-10 lg:mt-0 xl:mt-0 '>
-                  <p className='text-xxs lg:text-xs xs:text-xxs sm:text-xxs md:text-xxs font-bold  lg:ml-2.5'>
-                    {product.price}
-                  </p>
-                </div>
-                <button
-                  onClick={() => dispatch(removeFromCart(product.id))}
-                  className=' flex text-black-200   '
-                >
-                  <AiOutlineClose className='text-lg text-lg xs:text-xs sm:text-xs md:text-xs lg:text-sm xs:mt-1 sm:mt-1 md:mt-1 lg:mt-0 ' />
-                </button>
               </div>
             ))}
           </div>
