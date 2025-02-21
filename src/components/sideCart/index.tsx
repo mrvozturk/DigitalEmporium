af7526c8482ba54ea5b108fd2f57ebafc20f0b35
Merve Ozturk <ozturkmerve745@gmail.com>
2025-02-21 15:58:18 +0300

    sidecart couponinput file added

--- src/components/sideCart/index.tsx
+++ src/components/sideCart/index.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '@/lib/store';
 import { removeFromCart } from '@/lib/features/cart/cartSlice';
@@ -10,6 +10,7 @@ import {
   AiOutlineEdit
 } from 'react-icons/ai';
 import Image from 'next/image';
+import CouponInput from '../couponInput';
 
 interface SideCartProps {
   isOpen: boolean;
@@ -19,9 +20,8 @@ interface SideCartProps {
 const SideCart: React.FC<SideCartProps> = ({ isOpen, onClose }) => {
   const products = useSelector((state: RootState) => state.cart.items);
   const dispatch = useDispatch();
-  const [isCouponInputVisible, setIsCouponInputVisible] = useState(false);
 
-  if (!isOpen || products.length === 0) return null;
+  if (!isOpen) return null;
 
   // Ürün fiyatları hesaplama
   const total = products.reduce((acc, item) => {
@@ -36,128 +36,106 @@ const SideCart: React.FC<SideCartProps> = ({ isOpen, onClose }) => {
     <div
       className={`fixed right-5 w-96 h-[650px] bg-white shadow-2xl rounded-lg transform mt-1 ${
         isOpen ? 'scale-100 opacity-100' : 'scale-90 opacity-0'
-      } transition-all duration-300 z-50 flex flex-col xs:hidden md:flex`}
+      } transition-all duration-300 z-50 flex flex-col`}
     >
       {/* Sepet Başlık ve Kapatma Butonu */}
-      <div className='flex items-center justify-between p-5'>
+      <div className='flex items-center justify-between p-5 border-b'>
         <h2 className='text-lg font-bold'>Sepet ({products.length})</h2>
-        <button onClick={onClose} className='text-black-600 hover:text-black'>
+        <button onClick={onClose} className='hover:text-black'>
           <AiOutlineClose size={24} />
         </button>
       </div>
 
-      {/* Ücretsiz Kargo Bilgisi */}
-      <div className='flex flex-col items-center text-center px-6 py-3'>
-        <div className='border-t border-b border-gray-200 w-full flex flex-col items-center py-3'>
-          <p className='text-sm font-bold text-green-700'>
-            ÜCRETSİZ STANDART GÖNDERİ!
-          </p>
-          <p className='text-xs text-black'>
-            Adrese ücretsiz standart gönderi fırsatı elde ettin
-          </p>
+      {products.length === 0 ? (
+        <div className='flex items-center justify-center flex-1 text-gray-500'>
+          Sepetiniz boş
         </div>
-      </div>
+      ) : (
+        <>
+          {/* Ücretsiz Kargo Bilgisi */}
+          <div className='px-6 py-3 border-b text-center'>
+            <p className='text-sm font-bold text-green-700'>
+              ÜCRETSİZ STANDART GÖNDERİ!
+            </p>
+            <p className='text-xs text-black'>
+              Adrese ücretsiz standart gönderi fırsatı elde ettin
+            </p>
+          </div>
 
-      {/* Sepet Ürünleri */}
-      <div className='p-5 overflow-y-auto flex-1 max-h-[400px]'>
-        {products.map(product => (
-          <div key={product.id} className='flex mb-3 pb-3 w-full '>
-            <div className='flex items-start justify-between  w-full xs:w-40 sm:w-[82px] md:w-[80px] lg:w-[80px]  '>
-              <Image
-                src={product.src}
-                alt={product.title}
-                width={120}
-                height={120}
-                className='object-contain border border-gray-300 rounded-md shadow-md p-1 w-[80px] h-[150px]'
-              />
-            </div>
-            {/* Ürün Bilgileri ve Butonlar */}
-            <div className='flex-1 flex flex-col justify-between px-3'>
-              <div>
-                <h3 className='text-sm xs:text-xs sm:text-xs md:text-xs lg:text-xs  mb-1'>
-                  {product.title}
-                </h3>
-                <p className='text-xs font-bold mb-2'>{product.price}</p>
-                <p className='text-xs text-gray-500'>1 adet | M | Orta gri</p>
+          {/* Sepet Ürünleri */}
+          <div className='p-5 overflow-y-auto flex-1 max-h-[350px]'>
+            {products.map(product => (
+              <div key={product.id} className='flex mb-3 pb-3 border-b'>
+                <Image
+                  src={product.src}
+                  alt={product.title}
+                  width={80}
+                  height={120}
+                  className='object-contain border border-gray-300 rounded-md shadow-md p-1 w-[80px] h-[120px]'
+                />
+                <div className='flex-1 flex flex-col justify-between px-3'>
+                  <div>
+                    <h3 className='text-sm mb-1'>{product.title}</h3>
+                    <p className='text-xs font-bold mb-2'>{product.price}</p>
+                    <p className='text-xs text-gray-500'>
+                      {product.quantity} adet | M | Orta gri
+                    </p>
+                  </div>
+                  <div className='flex justify-end space-x-3 mt-4'>
+                    <button className='w-8 h-8 flex items-center justify-center bg-gray-100 rounded-full hover:bg-red-200'>
+                      <AiOutlineHeart size={18} />
+                    </button>
+                    <button className='w-8 h-8 flex items-center justify-center bg-gray-100 rounded-full hover:bg-red-200'>
+                      <AiOutlineEdit size={18} />
+                    </button>
+                    <button
+                      onClick={() => dispatch(removeFromCart(product.id))}
+                      className='w-8 h-8 flex items-center justify-center bg-gray-100 rounded-full hover:bg-red-200 text-black-600'
+                    >
+                      <AiOutlineDelete size={18} />
+                    </button>
+                  </div>
+                </div>
               </div>
-
-              <div className='flex justify-end space-x-3 mt-4'>
-                <button className='w-8 h-8  flex items-center justify-center bg-gray-100 rounded-full hover:bg-red-200'>
-                  <AiOutlineHeart
-                    size={18}
-                    className='w-6 h-6  xs:w-4 xs:h-4 sm:w-4 sm:h-4 md:w-4 md:h-4 lg:w-4 lg:h-4'
-                  />
-                </button>
-                <button className='w-8 h-8 flex items-center justify-center bg-gray-100 rounded-full hover:bg-red-200'>
-                  <AiOutlineEdit
-                    size={18}
-                    className='w-4 h-4  xs:w-4 xs:h-4 sm:w-4 sm:h-4 md:w-4 md:h-4 lg:w-4 lg:h-4'
-                  />
-                </button>
-                <button
-                  onClick={() => dispatch(removeFromCart(product.id))}
-                  className='w-8 h-8 flex items-center justify-center bg-gray-100 rounded-full hover:bg-red-200 text-black-600'
-                >
-                  <AiOutlineDelete
-                    size={18}
-                    className='w-6 h-6  xs:w-4 xs:h-4 sm:w-4 sm:h-4 md:w-4 md:h-4 lg:w-4 lg:h-4'
-                  />
-                </button>
-              </div>
+            ))}
+            {/* Kupon Alanı */}
+            <div className='text-xs'>
+              <CouponInput />
             </div>
           </div>
-        ))}
 
-        {/* Kampanya Kodu Alanı */}
-        <div className='flex justify-between items-center border-b border-gray-200 py-3'>
-          <p className='text-sm'>Kampanya kodu</p>
-          <button
-            className='text-black-600 text-xs font-bold'
-            onClick={() => setIsCouponInputVisible(!isCouponInputVisible)}
-          >
-            {isCouponInputVisible ? 'Kapat' : 'Ekle'}
-          </button>
-        </div>
-
-        {isCouponInputVisible && (
-          <input
-            type='text'
-            className='w-full border px-3 py-2 mt-2 rounded-md text-sm'
-            placeholder='Kodu gir'
-          />
-        )}
-      </div>
-
-      {/* Sipariş Bölümü */}
-      <div className='p-5 bg-white'>
-        <div className='flex justify-between text-base mb-3'>
-          <span>Alt toplam ({products.length} ürün)</span>
-          <span>
-            {total.toLocaleString('tr-TR', {
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2
-            })}
-            TL
-          </span>
-        </div>
-        <div className='text-sm flex justify-between text-base mb-3'>
-          <span>Kargo ücreti</span>
-          <span className='text-sm text-green-600 font-semibold'>ÜCRETSİZ</span>
-        </div>
-        <div className='flex justify-between text-sm font-bold mb-3'>
-          <span>Toplam (KDV dahil)</span>
-          <span>
-            {total.toLocaleString('tr-TR', {
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2
-            })}
-            TL
-          </span>
-        </div>
-        <button className='w-full bg-green-600 text-white py-3 rounded-md hover:bg-green-700 text-center font-semibold text-lg'>
-          SİPARİŞİ İŞLEME AL
-        </button>
-      </div>
+          {/* Sipariş Bilgileri */}
+          <div className='p-5 border-t bg-white'>
+            <div className='flex justify-between text-base mb-3'>
+              <span>Alt toplam ({products.length} ürün)</span>
+              <span>
+                {total.toLocaleString('tr-TR', {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2
+                })}
+                TL
+              </span>
+            </div>
+            <div className='flex justify-between text-base mb-3'>
+              <span>Kargo ücreti</span>
+              <span className='text-green-600 font-semibold'>ÜCRETSİZ</span>
+            </div>
+            <div className='flex justify-between text-sm font-bold mb-3'>
+              <span>Toplam (KDV dahil)</span>
+              <span>
+                {total.toLocaleString('tr-TR', {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2
+                })}
+                TL
+              </span>
+            </div>
+            <button className='w-full bg-green-600 text-white py-3 rounded-md hover:bg-green-700 text-center font-semibold text-lg'>
+              SİPARİŞİ İŞLEME AL
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
