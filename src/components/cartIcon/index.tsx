f0a824097977f1ce811f30a22219cebe0e371acb
Merve Ozturk <ozturkmerve745@gmail.com>
2025-02-28 16:52:30 +0300

    css updated

--- src/components/cartIcon/index.tsx
+++ src/components/cartIcon/index.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import React, { useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '@/lib/store';
@@ -9,13 +8,18 @@ import { AiOutlineShoppingCart } from 'react-icons/ai';
 import SideCart from './../sideCart';
 
 const CartIcon = () => {
-  const products = useSelector((state: RootState) => state.cart.items);
+  const productCount = useSelector(
+    (state: RootState) => state.cart.items.length
+  );
   const isCartOpen = useSelector((state: RootState) => state.cart.isCartOpen);
   const dispatch = useDispatch();
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   const handleMouseEnter = () => {
-    if (products.length > 0) dispatch(toggleCart(true));
+    if (productCount > 0) {
+      if (timeoutRef.current) clearTimeout(timeoutRef.current);
+      dispatch(toggleCart(true));
+    }
   };
 
   const handleMouseLeave = () => {
@@ -26,7 +30,7 @@ const CartIcon = () => {
 
   return (
     <div
-      className='relative md:hover:block'
+      className='relative'
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
@@ -35,26 +39,26 @@ const CartIcon = () => {
         href='/cart'
         className='flex items-center hover:text-gray-600 relative'
       >
-        <span className='hidden md:block mr-6'>Sepet [{products.length}]</span>
+        <span className='hidden md:block mr-6'>Sepet [{productCount}]</span>
         <div className='relative md:hidden'>
           <AiOutlineShoppingCart className='text-xl' />
-          {products.length > 0 && (
+          {productCount > 0 && (
             <span className='absolute -top-1 -right-1 bg-red-600 text-white text-[10px] font-bold w-4 h-4 flex items-center justify-center rounded-full'>
-              {products.length}
+              {productCount}
             </span>
           )}
         </div>
       </Link>
 
       {/* SideCart */}
-      <div className='hidden md:block absolute top-12 right-0 w-[350px] z-50'>
-        {isCartOpen && products.length > 0 && (
+      {isCartOpen && productCount > 0 && (
+        <div className='hidden md:block absolute top-12 right-0 w-[350px] z-50'>
           <SideCart
             isOpen={isCartOpen}
             onClose={() => dispatch(toggleCart(false))}
           />
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
