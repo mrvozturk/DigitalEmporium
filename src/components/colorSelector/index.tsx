cbed0c4a974c40d5bdb2690087f4b4298ac13934
Merve Ozturk <ozturkmerve745@gmail.com>
2025-05-11 00:28:18 +0300

    Double slash usage removed edited

--- src/components/colorSelector/index.tsx
+++ src/components/colorSelector/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { VariantColor } from '@/lib//types/product';
+import { VariantColor } from '@/lib/types/product';
 
 interface ColorSelectorProps {
   colors: VariantColor[];
