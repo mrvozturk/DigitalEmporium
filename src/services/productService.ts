d23eb515e7e93ca7ca2e71ab4b1ac97a79b069b4
Merve Ozturk <ozturkmerve745@gmail.com>
2025-08-18 19:07:08 +0300

    updated

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -5,7 +5,7 @@ import {
 } from '../lib/types/product';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL
-  ? `${process.env.NEXT_PUBLIC_API_URL}api/v1/products`
+  ? `${process.env.NEXT_PUBLIC_API_URL}products`
   : 'https://postresql-api-pink.vercel.app/api/v1/products';
 
 const HEADERS = {
